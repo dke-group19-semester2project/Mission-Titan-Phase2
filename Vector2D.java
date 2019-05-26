c09a042142d945ab78d2408912de491de13016bc
stephanpossen <43991595+stephanpossen@users.noreply.github.com>
2019-05-26 20:38:54 +0200

    Just added functionality, did not change any existing code

--- Vector2D.java
+++ Vector2D.java
@@ -25,6 +25,38 @@ public class Vector2D {
         String newString = "X = " + x + "   Y = " + y;
         return newString;
     }
+    
+    public Vector2D addVector(Vector2D v) {
+        return new Vector2D(this.x + v.x, this.y + v.y);
+    }
+
+    public Vector2D addConstant(double d) {
+        return new Vector2D(this.x + d, this.y + d);
+    }
+
+    public Vector2D substractVector(Vector2D v) {
+        return new Vector2D(this.x - v.x, this.y - v.y);
+    }
+
+    public Vector2D substractConstant(double d) {
+        return new Vector2D(this.x - d, this.y - d);
+    }
+
+    public Vector2D multiplyVector(Vector2D v) {
+        return new Vector2D(this.x * v.x, this.y * v.y);
+    }
+
+    public Vector2D multiplyConstant(double d) {
+        return new Vector2D(this.x * d, this.y * d);
+    }
+
+    public Vector2D divideVector(Vector2D v) {
+        return new Vector2D(this.x / v.x, this.y / v.y);
+    }
+
+    public Vector2D divideConstant(double d) {
+        return new Vector2D(this.x / d, this.y / d);
+    }
     public double getX(){
         return x;
     }
