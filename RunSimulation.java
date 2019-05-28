c3ebfd33cfd89e51bea0623a75ff87807161ceab
Laura Isotalo <l.isotalo@student.maastrichtuniversity.nl>
2019-05-28 12:18:31 +0200

    Added method to run CL simulation

--- RunSimulation.java
+++ RunSimulation.java
@@ -1,10 +1,12 @@
 public class RunSimulation {
     public static void main(String[] args) {
-        displayInternalSimulation();
 
-//        for (int i = 0; i<1000; i++) {
-           // displayOpenLoopSimulation();
-//        }
+
+        for (int i = 0; i<1000; i++) {
+            //displayInternalSimulation();
+            displayClosedLoopSimulation();
+           //displayOpenLoopSimulation();
+        }
 
 
 
@@ -33,8 +35,17 @@ public class RunSimulation {
         openLoopSim.run();
     }
     public static void displayClosedLoopSimulation () {
-        /*
-        TODO: Implement once feedback controller is available
-         */
+        final double titanRadius = 2575*1000;
+        final double startingDistance = titanRadius+800*1000;
+        final double probeMass = 5000;
+        WindSpeedInterface stochasticWind = new WindSpeedStochastic(1);
+        ((WindSpeedStochastic) stochasticWind).setRandomParameter(0.5);
+        SimulationBody titan = new SimulationBody(new Vector2D(0,0), new Vector2D(0,0),
+                1.3452E23, 2*titanRadius,
+                new WindSpeedStochastic(1));
+        SimulationBody realProbe = new SimulationBody(new Vector2D(startingDistance,0), new Vector2D(0, 1600),
+                5000, 1, stochasticWind);
+        Simulation closedLoopSim = new Simulation(new FeedbackController(), titan, realProbe);
+        closedLoopSim.run();
     }
 }
