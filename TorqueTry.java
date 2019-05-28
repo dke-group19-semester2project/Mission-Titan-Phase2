5ba15ed6b1d6fac2fc625bd2134c2849453e471e
KarimEsch <45858208+KarimEsch@users.noreply.github.com>
2019-05-28 09:39:51 +0200

    Update TorqueTry.java

--- TorqueTry.java
+++ TorqueTry.java
@@ -6,37 +6,18 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class TorqueTry extends JComponent {
+public class TorqueTry{
     private double force;
     private double angularVelocity;
     private double angularAcceleration;
     private double position;
     private double angle;
+    private SimulationBody object;
     //distance are in meters
     private final double radius =6;
-    public static void main(String[]args){
-        JFrame frame = new JFrame ();
-        TorqueTry movingSpaceCraft = new TorqueTry ();
-        class CalculateListener implements ActionListener {
-            public void actionPerformed(ActionEvent event) {
-                double rate = Double.parseDouble(rateField.getText());
-                account.deposit((account.getBalance()*rate/100));
-                textArea.append(account.getBalance() + "\n");
-        }
-        }
-        ActionListener listener = new CalculateListener();
-        frame.add(movingSpaceCraft);
-        frame.setSize ( 800,800 );
-        frame.setDefaultCloseOperation (WindowConstants.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-    }
-    public void paintComponent (Graphics g){
-        Graphics2D g2 = (Graphics2D) g;
-        // Construct a rectangle and draw it
-        Rectangle box = new Rectangle(5, 10, 20, 30);
-        box.(395, 395);
-        g2.draw(box);
 
+    TorqueTry(SimulationBody object){
+        this.object=object;
     }
     /*
     public double updatedAngle (double force,double angle){
@@ -61,6 +42,10 @@ public class TorqueTry extends JComponent {
 
 
     //reference for the power of the thrusters
+    private float calculateTorque() {
+        var distToCOM = shape.localCOM.mul( -1.0);
+        return distToCOM.x * thrustDir.y - radius * thrustDir.x;
+    }
     public void leftThrust()
     {
         oldAngle=angle;
@@ -70,45 +55,46 @@ public class TorqueTry extends JComponent {
         spaceCraft.transform(new Affine(new Rotate(Spacecraft.leftThrust, x, y)));
 
     }
-    public void useLeftThrusters (double force){
+    public void useLeftThrusters (){
         oldAngle=angle;
         angle += Math.PI / 180;
-        spaceCraft.transform(new Affine(new Rotate(Spacecraft.leftThrust, x, y)));
+        spaceCraft.transform(new Affine(new Rotate(object.leftThrust, x, y)));
     }
-    public void useLeftThrusters (double force){
+    public void useRightThrusters (){
         oldAngle=angle;
         angle -= Math.PI / 180;
-        spaceCraft.transform(new Affine(new Rotate(Spacecraft.rightThrust, x, y)));
+        object.transform(new Affine(new Rotate(TorqueTry(object).rightThrust, x, y)));
     }
     public void leftThrustAction()
     {
-        double a = //value of the engine
-        double vel=a* //a timestep;
-        double distance = vel*// a timestep;
-        double dX = Math.sin(-(angle + (Math.PI/2)))*distance;
-        double dY = Math.cos(angle + Math.PI/2)*distance;
-        velX += dX/ //a timestep;
-        velY += dY/ //a timestep;
+        double a = object.getForce ().getEuclideanLength ()/object.getMassInKg ();
+        double vel=a;
+        double radius = vel;
+        double dX = Math.sin(-(angle + (Math.PI/2)))*radius;
+        double dY = Math.cos(angle + Math.PI/2)*radius;
+        velX += dX;
+        velY += dY;
+        delaV=velX + velY;
     }
     public void rightThrustAction()
     {
-        double a = //value of the engine
-        double vel=a* //a timestep;
-        double distance = vel*// a timestep;
-        double dX = Math.sin((angle + (Math.PI/2)))*distance;
-        double dY = Math.cos(angle + Math.PI/2)*distance;
-        velX += dX/ //a timestep;
-        velY += dY/ //a timestep;
+        double a = object.getForce ().getEuclideanLength ()/object.getMassInKg ();
+        double vel=a;
+        double radius = vel;
+        double dX = Math.sin((angle + (Math.PI/2)))*radius;
+        double dY = Math.cos(angle + Math.PI/2)*radius;
+        velX += dX;
+        velY += dY;
     }
 
     public void mainThrust()
     {
-        double a = //value of the engine
-        double vel=a* //a timestep;
-        double distance = vel* //a timestep;
-        double dX = Math.sin(-angle)*distance;
-        double dY = Math.cos(angle)*distance;
-        velX += dX/ //a timestep;
-        velY += dY/ //a timestep;
+        double a = object.getForce ().getEuclideanLength ()/object.getMassInKg ();
+        double vel=a;
+        double radius = vel;
+        double dX = Math.sin(-angle)*radius;
+        double dY = Math.cos(angle)*radius;
+        velX += dX;
+        velY += dY;
     }
 }
