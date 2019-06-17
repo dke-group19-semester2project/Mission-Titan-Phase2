0e2ea0b587cb819b8f50496928c468b55d645520
stephanpossen <43991595+stephanpossen@users.noreply.github.com>
2019-06-17 11:51:53 +0200

    Update ControllerInterface.java
    Added getter method for force used

--- ControllerInterface.java
+++ ControllerInterface.java
@@ -3,4 +3,5 @@ public interface ControllerInterface {
     public Vector2D updateAndGetDeltaV (Simulation sim);
     public Vector2D convertDeltaVToForce (Vector2D deltaV);
     public double convertDeltaVToForceMagnitude (Vector2D deltaV);
+    public double getForceUsed();
 }
